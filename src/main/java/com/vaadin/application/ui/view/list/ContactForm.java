e2db157bacedf9a6e7d8fef9bb160f7176db3b81
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-11-16 14:24:53 +0300

    adding page navigation and sidebar

---
+++ src/main/java/com/vaadin/application/ui/view/list/ContactForm.java
@@ -0,0 +1,145 @@
+package com.vaadin.application.ui.view.list;
+
+import java.util.List;
+
+import com.vaadin.application.backend.entity.Company;
+import com.vaadin.application.backend.entity.Contact;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.shared.Registration;
+
+public class ContactForm extends FormLayout {
+    private Contact contact;
+
+    TextField firstName = new TextField("First name");
+    TextField lastName = new TextField("Last name");
+    EmailField email = new EmailField("Email");
+    ComboBox<Contact.Status> status = new ComboBox<>("Status");
+    ComboBox<Company> company = new ComboBox<>("Company");
+    Button save = new Button("Save");
+    Button delete = new Button("Delete");
+    Button close = new Button("Cancel");
+
+    //creating a binderthat is aware of bean validation annotations(beanvalidationbinder)
+    //by passing it in Contact.class we define the object we're binding to
+    Binder<Contact> binder = new BeanValidationBinder<>(Contact.class);
+
+
+
+    public ContactForm(List<Company> companies) {
+        //bindInstanceFields matches fields in Contact and ContactForm base on names
+
+            addClassName("contact-form");
+            binder.bindInstanceFields(this);
+            company.setItems(companies);
+            company.setItemLabelGenerator(Company::getName);
+            status.setItems(Contact.Status.values());
+
+
+            //form structure
+        addClassName("contact-form");
+        add(firstName,
+                lastName,
+                email,
+                company,
+                status,
+                createButtonsLayout());
+    }
+
+//    private HorizontalLayout createButtonsLayout() {
+//
+//
+//
+//
+//    }
+
+    public void setContact(Contact contact){
+        //Save a reference to the contact, so we can save the form values back into it later.
+        this.contact = contact;
+        //Calls binder.readBean to bind the values from the contact to the UI fields. readBen
+        //copies the values from the Contact to an internal model, that way we don’t
+        //accidentally overwrite values if we cancel editing
+        binder.readBean(contact);
+    }
+
+    //saving, deleting, and closing the form
+    private Component createButtonsLayout() {
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        save.addClickShortcut(Key.ENTER);
+        close.addClickShortcut(Key.ESCAPE);
+
+        // The save button calls the validateAndSave method
+        save.addClickListener(event -> validateAndSave());
+        // The delete button fires a delete event and passes the currently-edited contact
+        delete.addClickListener(event -> fireEvent(new DeleteEvent(this, binder.getBean())));
+        // The cancel button fires a close event.
+        close.addClickListener(event -> fireEvent(new CloseEvent(this)));
+        // Validates the form every time it changes. If it is invalid, it disables the save button to
+        // avoid invalid submissions
+        binder.addStatusChangeListener(e -> save.setEnabled(binder.isValid()));
+
+        return new HorizontalLayout(save, delete, close);
+    }
+
+    // Events
+    public static abstract class ContactFormEvent extends ComponentEvent<ContactForm> {
+        private Contact contact;
+        //ContactFormEvent is a common superclass for all the events. It contains the contact
+        //that was edited or deleted
+        protected ContactFormEvent(ContactForm source, Contact contact) {
+            super(source, false);
+            this.contact = contact;
+        }
+        public Contact getContact() {
+            return contact;
+        }
+    }
+    public static class SaveEvent extends ContactFormEvent {
+        SaveEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+    public static class DeleteEvent extends ContactFormEvent {
+        DeleteEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+    public static class CloseEvent extends ContactFormEvent {
+        CloseEvent(ContactForm source) {
+            super(source, null);
+        }
+    }
+    // The addListener method uses Vaadin’s event bus to register the custom event types
+    public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType,
+                                                                  ComponentEventListener<T> listener) {
+        return getEventBus().addListener(eventType, listener);
+    }
+
+
+
+    private void validateAndSave() {
+        try {
+            //⑤ Write the form contents back to the original contact.
+            binder.writeBean(contact);
+            //⑥ Fire a save event so the parent component can handle the action.
+            fireEvent(new SaveEvent(this, contact));
+        } catch (ValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/com/vaadin/application/ui/view/list/ListView.java
@@ -0,0 +1,145 @@
+package com.vaadin.application.ui.view.list;
+
+import com.vaadin.application.backend.entity.Company;
+import com.vaadin.application.backend.entity.Contact;
+import com.vaadin.application.backend.service.CompanyService;
+import com.vaadin.application.backend.service.ContactService;
+import com.vaadin.application.ui.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+
+@Route(value="", layout = MainLayout.class)
+@PageTitle("Contacts | Vaadin CRM")
+public class ListView extends VerticalLayout {
+    private ContactService contactService;
+    private Grid<Contact> grid = new Grid<>(Contact.class);
+    private TextField filterText = new TextField();
+
+    //import contact form
+    private ContactForm form;
+    public ListView(ContactService contactService, CompanyService companyservice) {
+        this.contactService = contactService;
+        addClassName("list-view");
+        setSizeFull();
+        //getToolBar();
+        configureGrid();
+
+        //call form constructor
+        form = new ContactForm(companyservice.findAll());
+        form.addListener(ContactForm.SaveEvent.class, this::saveContact);
+        form.addListener(ContactForm.DeleteEvent.class, this::deleteContact);
+        form.addListener(ContactForm.CloseEvent.class, e -> closeEditor());
+
+        //Creates a Div that wraps the grid and the form, gives it a CSS class name, and makes
+        //it full size
+        Div content = new Div(grid, form);
+        content.addClassName("content");
+        content.setSizeFull();
+
+        // Adds the content layout to the main layout
+        add(getToolBar(), content);
+        updateList();
+
+        //① The closeEditor() call at the end of the constructor Sets the form contact to null,
+        //clearing out old values. Hides the form. Removes the "editing" CSS class from the
+        //view.
+        closeEditor();
+    }
+
+    private void deleteContact(ContactForm.DeleteEvent event) {
+        contactService.delete(event.getContact());
+        updateList();
+        closeEditor();
+
+    }
+
+    //saveContact method
+    private void saveContact(ContactForm.SaveEvent event) {
+        contactService.save(event.getContact());
+        updateList();
+        closeEditor();
+    }
+
+    //text field method
+    private HorizontalLayout getToolBar() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
+
+
+        //add button
+        Button addContactButton = new Button("Add Contact", click -> addContact());
+
+
+        HorizontalLayout toolbar = new HorizontalLayout(filterText, addContactButton);
+        toolbar.addClassName("toolbar");
+        return toolbar;
+    }
+
+    private void addContact() {
+        grid.asSingleSelect().clear();
+        editContact(new Contact());
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.removeColumnByKey("company");
+        grid.setColumns("firstName", "lastName", "email", "status");
+        grid.addColumn(contact -> {
+            Company company = contact.getCompany();
+            return company == null ? "-" : company.getName();
+        }).setHeader("Company");
+
+        //column sizing to aouto
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+        //② addValueChangeListener adds a listener to the grid. The Grid component supports
+        //multi and single-selection modes. We only want to select a single Contact, so we use
+        //the asSingleSelect() method. The getValue() method returns the Contact in the
+        //selected row or null if there’s no selection
+        grid.asSingleSelect().addValueChangeListener(event ->
+                editContact(event.getValue()));
+    }
+
+
+
+    //③ editContact sets the selected contact in the ContactForm and hides or shows the
+    //form, depending on the selection. It also sets the "editing" CSS class name when
+    //editing
+    public void editContact(Contact contact) {
+        if (contact == null) {
+            closeEditor();
+        } else {
+            form.setContact(contact);
+            form.setVisible(true);
+            addClassName("editing");
+        }
+    }
+    private void closeEditor() {
+        form.setContact(null);
+        form.setVisible(false);
+        removeClassName("editing");
+    }
+
+    private void updateList() {
+
+        grid.setItems(contactService.findAll(filterText.getValue()));
+    }
+
+
+
+
+
+
+}
